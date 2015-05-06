5d6814ec49b53c2652c16f2bb1bfa2a243c8cf36
Emmanuel Keller <ekeller@open-search-server.com>
2015-05-07 00:24:15 +0200

    Fix path issues

--- src/main/java/com/qwazr/crawler/web/WebCrawlerServer.java
+++ src/main/java/com/qwazr/crawler/web/WebCrawlerServer.java
@@ -73,7 +73,7 @@ public class WebCrawlerServer extends AbstractServer {
 	@Override
 	public void load() throws IOException {
 		ClusterServer.load(getWebServicePublicAddress(), getCurrentDataDir(),
-				null, null);
+				null);
 		ScriptManager.load(getCurrentDataDir());
 		load(this);
 	}
