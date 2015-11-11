2913f2fd475544a167134fe2884e89dd18b08931
Emmanuel Keller <ekeller@open-search-server.com>
2015-11-12 00:32:48 +0100

    Crawl stay in main window

--- src/main/java/com/qwazr/crawler/web/driver/HtmlUnitBrowserDriver.java
+++ src/main/java/com/qwazr/crawler/web/driver/HtmlUnitBrowserDriver.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2015 Emmanuel Keller / QWAZR
- * <p/>
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,111 +33,114 @@ import static com.qwazr.utils.json.JsonMapper.MAPPER;
 
 public class HtmlUnitBrowserDriver extends BrowserDriver<HtmlUnitDriverWebClient> {
 
-    private List<InputStream> inputStreamList = null;
+	private List<InputStream> inputStreamList = null;
 
-    public HtmlUnitBrowserDriver(Capabilities capabilities) {
-	super(BrowserDriverEnum.html_unit, capabilities);
-    }
-
-    @Override
-    public HtmlUnitDriverWebClient initialize(Capabilities capabilities) {
-	if (capabilities == null)
-	    return new HtmlUnitDriverWebClient();
-	else
-	    return new HtmlUnitDriverWebClient(capabilities);
-    }
-
-    @Override
-    public void close() {
-	if (inputStreamList != null) {
-	    for (InputStream inputStream : inputStreamList)
-		IOUtils.closeQuietly(inputStream);
-	    inputStreamList.clear();
+	public HtmlUnitBrowserDriver(Capabilities capabilities) {
+		super(BrowserDriverEnum.html_unit, capabilities);
 	}
-	super.close();
-    }
 
-    public WebClient getWebClient() {
-	return driver.getWebClient();
-    }
-
-    public Page getEnclosedPage() {
-	WebClient webClient = getWebClient();
-	if (webClient == null)
-	    return null;
-	WebWindow webWindow = webClient.getCurrentWindow();
-	if (webWindow == null)
-	    return null;
-	return webWindow.getEnclosedPage();
-    }
-
-    public WebResponse getWebResponse() {
-	Page page = getEnclosedPage();
-	if (page == null)
-	    return null;
-	return page.getWebResponse();
-    }
-
-    public Integer getStatusCode() {
-	WebResponse response = getWebResponse();
-	if (response == null)
-	    return null;
-	return response.getStatusCode();
-    }
-
-    public String getContentType() {
-	WebResponse response = getWebResponse();
-	if (response == null)
-	    return null;
-	return response.getContentType();
-    }
-
-    public boolean isUnexpectedPage() {
-	Page page = getEnclosedPage();
-	if (page == null)
-	    return false;
-	return page instanceof UnexpectedPage;
-    }
-
-    public InputStream getInputStream() throws IOException {
-	WebResponse response = getWebResponse();
-	if (response == null)
-	    return null;
-	if (inputStreamList == null)
-	    inputStreamList = new ArrayList<InputStream>();
-	InputStream inputStream = response.getContentAsStream();
-	if (inputStream == null)
-	    inputStreamList.add(inputStream);
-	return inputStream;
-    }
-
-    public void saveResponseFile(File file) throws IOException {
-	InputStream inputStream = getInputStream();
-	if (inputStream == null)
-	    return;
-	try {
-	    FileOutputStream fos = new FileOutputStream(file);
-	    try {
-		IOUtils.copy(inputStream, fos);
-	    } finally {
-		IOUtils.close(fos);
-	    }
-	} finally {
-	    IOUtils.close(inputStream);
+	@Override
+	public HtmlUnitDriverWebClient initialize(Capabilities capabilities) {
+		if (capabilities == null)
+			return new HtmlUnitDriverWebClient();
+		else
+			return new HtmlUnitDriverWebClient(capabilities);
 	}
-    }
 
-    public void saveResponse(String path) throws IOException {
-	saveResponseFile(new File(path));
-    }
+	@Override
+	public void close() {
+		if (inputStreamList != null) {
+			for (InputStream inputStream : inputStreamList)
+				IOUtils.closeQuietly(inputStream);
+			inputStreamList.clear();
+		}
+		super.close();
+	}
 
-    public Page request(String json) throws IOException {
-	WebRequestDefinition webRequestDef = MAPPER.readValue(json, WebRequestDefinition.class);
-	return getWebClient().getPage(webRequestDef.getNewWebRequest());
-    }
+	public WebClient getWebClient() {
+		return driver.getWebClient();
+	}
 
-    public WebElement convertNode(DomElement node) {
-	return driver.convertNode(node);
-    }
+	public Page getEnclosedPage() {
+		WebClient webClient = getWebClient();
+		if (webClient == null)
+			return null;
+		WebWindow webWindow = webClient.getCurrentWindow();
+		if (webWindow == null)
+			return null;
+		return webWindow.getEnclosedPage();
+	}
+
+	public void test() {
+		getWebClient().getCurrentWindow().getTopWindow();
+	}
+	public WebResponse getWebResponse() {
+		Page page = getEnclosedPage();
+		if (page == null)
+			return null;
+		return page.getWebResponse();
+	}
+
+	public Integer getStatusCode() {
+		WebResponse response = getWebResponse();
+		if (response == null)
+			return null;
+		return response.getStatusCode();
+	}
+
+	public String getContentType() {
+		WebResponse response = getWebResponse();
+		if (response == null)
+			return null;
+		return response.getContentType();
+	}
+
+	public boolean isUnexpectedPage() {
+		Page page = getEnclosedPage();
+		if (page == null)
+			return false;
+		return page instanceof UnexpectedPage;
+	}
+
+	public InputStream getInputStream() throws IOException {
+		WebResponse response = getWebResponse();
+		if (response == null)
+			return null;
+		if (inputStreamList == null)
+			inputStreamList = new ArrayList<InputStream>();
+		InputStream inputStream = response.getContentAsStream();
+		if (inputStream == null)
+			inputStreamList.add(inputStream);
+		return inputStream;
+	}
+
+	public void saveResponseFile(File file) throws IOException {
+		InputStream inputStream = getInputStream();
+		if (inputStream == null)
+			return;
+		try {
+			FileOutputStream fos = new FileOutputStream(file);
+			try {
+				IOUtils.copy(inputStream, fos);
+			} finally {
+				IOUtils.close(fos);
+			}
+		} finally {
+			IOUtils.close(inputStream);
+		}
+	}
+
+	public void saveResponse(String path) throws IOException {
+		saveResponseFile(new File(path));
+	}
+
+	public Page request(String json) throws IOException {
+		WebRequestDefinition webRequestDef = MAPPER.readValue(json, WebRequestDefinition.class);
+		return getWebClient().getPage(webRequestDef.getNewWebRequest());
+	}
+
+	public WebElement convertNode(DomElement node) {
+		return driver.convertNode(node);
+	}
 
 }
