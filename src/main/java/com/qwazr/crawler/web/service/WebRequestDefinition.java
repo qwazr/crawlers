a715a48238dc2c078f1d00fe4fbc11dbbc5ad088
Emmanuel Keller <ekeller@open-search-server.com>
2016-05-09 07:57:13 +0200

    Add builder

--- src/main/java/com/qwazr/crawler/web/service/WebRequestDefinition.java
+++ src/main/java/com/qwazr/crawler/web/service/WebRequestDefinition.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2014-2016 Emmanuel Keller / QWAZR
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,6 @@
 
 package com.qwazr.crawler.web.service;
 
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.gargoylesoftware.htmlunit.HttpMethod;
 import com.gargoylesoftware.htmlunit.WebRequest;
@@ -25,6 +24,7 @@ import com.gargoylesoftware.htmlunit.util.NameValuePair;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -56,6 +56,15 @@ public class WebRequestDefinition {
 		form_encoding_type = null;
 	}
 
+	private WebRequestDefinition(Builder builder) {
+		this.url = builder.url;
+		this.charset = builder.charset;
+		this.headers = builder.headers == null ? null : new LinkedHashMap<>(builder.headers);
+		this.parameters = builder.parameters == null ? null : new LinkedHashMap<>(builder.parameters);
+		this.method = builder.method;
+		this.form_encoding_type = builder.form_encoding_type;
+	}
+
 	public WebRequest getNewWebRequest() throws MalformedURLException {
 
 		WebRequest request = new WebRequest(new URL(url));
@@ -70,9 +79,8 @@ public class WebRequestDefinition {
 
 		// Set the request parameters
 		if (parameters != null) {
-			List<NameValuePair> requestParameters = new ArrayList<NameValuePair>(parameters.size());
-			for (Map.Entry<String, String> entry : parameters.entrySet())
-				requestParameters.add(new NameValuePair(entry.getKey(), entry.getValue()));
+			List<NameValuePair> requestParameters = new ArrayList<>(parameters.size());
+			parameters.forEach((key, value) -> requestParameters.add(new NameValuePair(key, value)));
 			request.setRequestParameters(requestParameters);
 		}
 
@@ -83,15 +91,77 @@ public class WebRequestDefinition {
 		// Set the form type
 		if (form_encoding_type != null) {
 			switch (form_encoding_type) {
-				case URL_ENCODED:
-					request.setEncodingType(com.gargoylesoftware.htmlunit.FormEncodingType.URL_ENCODED);
-					break;
-				case MULTIPART:
-					request.setEncodingType(com.gargoylesoftware.htmlunit.FormEncodingType.MULTIPART);
-					break;
+			case URL_ENCODED:
+				request.setEncodingType(com.gargoylesoftware.htmlunit.FormEncodingType.URL_ENCODED);
+				break;
+			case MULTIPART:
+				request.setEncodingType(com.gargoylesoftware.htmlunit.FormEncodingType.MULTIPART);
+				break;
 			}
 		}
 
 		return request;
+	}
+
+	public class Builder {
+
+		private String url;
+
+		private String charset;
+
+		private Map<String, String> headers;
+
+		private Map<String, String> parameters;
+
+		private HttpMethod method;
+
+		private FormEncodingType form_encoding_type;
+
+		public Builder(final String url) {
+			setUrl(url);
+			charset = null;
+			headers = null;
+			parameters = null;
+			method = HttpMethod.GET;
+			form_encoding_type = form_encoding_type.URL_ENCODED;
+		}
+
+		public Builder setUrl(final String url) {
+			this.url = url;
+			return this;
+		}
+
+		public Builder setCharset(final String charset) {
+			this.charset = charset;
+			return this;
+		}
+
+		public Builder addHeader(final String key, String value) {
+			if (headers == null)
+				headers = new LinkedHashMap<>();
+			headers.put(key, value);
+			return this;
+		}
+
+		public Builder addParameter(final String key, String value) {
+			if (parameters == null)
+				parameters = new LinkedHashMap<>();
+			parameters.put(key, value);
+			return this;
+		}
+
+		public Builder setHttpMethod(final HttpMethod method) {
+			this.method = method;
+			return this;
+		}
+
+		public Builder setFormEncodingType(final FormEncodingType formEncodingType) {
+			this.form_encoding_type = formEncodingType;
+			return this;
+		}
+
+		public WebRequestDefinition build() {
+			return new WebRequestDefinition(this);
+		}
 	}
 }
