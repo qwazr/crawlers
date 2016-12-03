c9f37587b4cd2186aa9e4297d112bc6386106a21
Emmanuel Keller <ekeller@open-search-server.com>
2016-12-03 20:58:44 +0100

    server generalisation

---
+++ src/main/java/com/qwazr/crawler/web/WebCrawlerServer.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2015-2016 Emmanuel Keller / QWAZR
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qwazr.crawler.web;
+
+import com.qwazr.classloader.ClassLoaderManager;
+import com.qwazr.cluster.manager.ClusterManager;
+import com.qwazr.crawler.web.manager.WebCrawlerManager;
+import com.qwazr.utils.server.GenericServer;
+import com.qwazr.utils.server.ServerBuilder;
+import com.qwazr.utils.server.ServerConfiguration;
+import com.qwazr.utils.server.WelcomeService;
+
+import java.io.IOException;
+
+public class WebCrawlerServer extends GenericServer {
+
+	private WebCrawlerServer(final ServerConfiguration serverConfiguration) throws IOException {
+		super(serverConfiguration);
+		final ServerBuilder builder = getBuilder();
+		builder.registerWebService(WelcomeService.class);
+		ClassLoaderManager.load(serverConfiguration.dataDirectory, null);
+		ClusterManager.load(builder);
+		WebCrawlerManager.load(builder);
+	}
+
+	public static void main(final String... args) throws Exception {
+		new WebCrawlerServer(new ServerConfiguration(args)).start(true);
+	}
+
+}
