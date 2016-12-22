ab633a3fd678c6ee1d32f6990501fc6991bd996d
Emmanuel Keller <ekeller@open-search-server.com>
2016-12-22 18:28:15 +0100

    Restore WebCrawlerTool

---
+++ src/main/java/com/qwazr/crawler/web/WebCrawlerTool.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2014-2016 Emmanuel Keller / QWAZR
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package com.qwazr.crawler.web;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.qwazr.crawler.web.driver.BrowserDriver;
+import com.qwazr.crawler.web.driver.BrowserDriverBuilder;
+import com.qwazr.crawler.web.service.WebCrawlDefinition;
+import com.qwazr.library.AbstractLibrary;
+import com.qwazr.utils.IOUtils;
+import com.qwazr.utils.json.JsonMapper;
+
+import java.io.IOException;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class WebCrawlerTool extends AbstractLibrary {
+
+	final public WebCrawlDefinition config = null;
+
+	@JsonIgnore
+	public WebCrawlDefinition getNewWebCrawlDefinition() {
+		return config == null ? new WebCrawlDefinition() : (WebCrawlDefinition) config.clone();
+	}
+
+	@JsonIgnore
+	public BrowserDriver getNewWebDriver(final IOUtils.CloseableContext context, final String json)
+			throws ReflectiveOperationException, SecurityException, IOException {
+		final WebCrawlDefinition webCrawlDef = JsonMapper.MAPPER.readValue(json, WebCrawlDefinition.class);
+		final BrowserDriver browserDriver = new BrowserDriverBuilder(webCrawlDef).build();
+		return context == null ? browserDriver : context.add(browserDriver);
+	}
+
+	@JsonIgnore
+	public BrowserDriver getNewWebDriver(final IOUtils.CloseableContext context) throws ReflectiveOperationException {
+		final BrowserDriver browserDriver = new BrowserDriverBuilder(config).build();
+		return context == null ? browserDriver : context.add(browserDriver);
+	}
+}
