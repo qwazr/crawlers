c59fd3122c5b17b8022bd080d0110243710a96ce
Emmanuel Keller <ekeller@open-search-server.com>
2018-04-16 12:26:48 +0200

    Fix windows incompatibilities

--- src/test/java/com/qwazr/crawler/file/FileCrawlManagerTest.java
+++ src/test/java/com/qwazr/crawler/file/FileCrawlManagerTest.java
@@ -21,65 +21,67 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class FileCrawlManagerTest {
 
-	private static ExecutorService executorService;
-	private static FileCrawlerManager crawlerManager;
+    private static ExecutorService executorService;
+    private static FileCrawlerManager crawlerManager;
 
-	@BeforeClass
-	public static void setup() {
-		executorService = Executors.newCachedThreadPool();
-		crawlerManager = new FileCrawlerManager(null, executorService);
-	}
+    @BeforeClass
+    public static void setup() {
+        executorService = Executors.newCachedThreadPool();
+        crawlerManager = new FileCrawlerManager(null, executorService);
+    }
 
-	@AfterClass
-	public static void cleanup() {
-		if (executorService != null)
-			executorService.shutdown();
-	}
+    @AfterClass
+    public static void cleanup() {
+        if (executorService != null)
+            executorService.shutdown();
+    }
 
-	FileCrawlDefinition.Builder getFileCrawlDefinition() {
-		return FileCrawlDefinition.of()
-				.entryPath("src/test/file_crawl")
-				.addInclusionPattern("*/")
-				.addInclusionPattern("*.txt")
-				.addExclusionPattern("*/ignore.*")
-				.addExclusionPattern("*/ignore/");
-	}
+    FileCrawlDefinition.Builder getFileCrawlDefinition() {
+        return FileCrawlDefinition.of()
+                .entryPath(Paths.get("src", "test", "file_crawl").toString())
+                .addInclusionPattern("*" + File.separator)
+                .addInclusionPattern("*.txt")
+                .addExclusionPattern("*" + File.separator + "ignore.*")
+                .addExclusionPattern("*" + File.separator + "ignore" + File.separator);
+    }
 
-	void crawlTest(FileCrawlDefinition.Builder fileCrawlDefinitionBuilder, int expectedCrawled, int expectedIgnored,
-			int expectedError) throws InterruptedException {
-		final String crawlSession = RandomUtils.alphanumeric(5);
-		FileCrawlStatus crawlStatus = crawlerManager.runSession(crawlSession, fileCrawlDefinitionBuilder.build());
-		Assert.assertNotNull(crawlStatus);
-		while (crawlStatus.endTime == null) {
-			crawlStatus = crawlerManager.getSession(crawlSession);
-			Thread.sleep(500);
-		}
-		Assert.assertEquals(expectedCrawled, crawlStatus.crawled);
-		Assert.assertEquals(expectedIgnored, crawlStatus.ignored);
-		Assert.assertEquals(expectedError, crawlStatus.error);
-		Assert.assertNotNull(crawlStatus.threadDone);
+    void crawlTest(FileCrawlDefinition.Builder fileCrawlDefinitionBuilder, int expectedCrawled, int expectedIgnored,
+                   int expectedError) throws InterruptedException {
+        final String crawlSession = RandomUtils.alphanumeric(5);
+        FileCrawlStatus crawlStatus = crawlerManager.runSession(crawlSession, fileCrawlDefinitionBuilder.build());
+        Assert.assertNotNull(crawlStatus);
+        while (crawlStatus.endTime == null) {
+            crawlStatus = crawlerManager.getSession(crawlSession);
+            Thread.sleep(500);
+        }
+        Assert.assertEquals(expectedCrawled, crawlStatus.crawled);
+        Assert.assertEquals(expectedIgnored, crawlStatus.ignored);
+        Assert.assertEquals(expectedError, crawlStatus.error);
+        Assert.assertNotNull(crawlStatus.threadDone);
 
-		if (expectedError == 0)
-			Assert.assertNull(crawlStatus.lastError);
-		else
-			Assert.assertNotNull(crawlStatus.lastError);
+        if (expectedError == 0)
+            Assert.assertNull(crawlStatus.lastError);
+        else
+            Assert.assertNotNull(crawlStatus.lastError);
 
-	}
+    }
 
-	@Test
-	public void crawlFullTest() throws InterruptedException {
-		crawlTest(getFileCrawlDefinition(), 7, 2, 0);
-	}
+    @Test
+    public void crawlFullTest() throws InterruptedException {
+        crawlTest(getFileCrawlDefinition(), 7, 2, 0);
+    }
 
-	@Test
-	public void crawlDepth() throws InterruptedException {
-		crawlTest(getFileCrawlDefinition().setMaxDepth(1), 4, 1, 0);
-		crawlTest(getFileCrawlDefinition().setMaxDepth(2), 6, 2, 0);
-		crawlTest(getFileCrawlDefinition().setMaxDepth(0), 1, 0, 0);
-	}
+    @Test
+    public void crawlDepth() throws InterruptedException {
+        crawlTest(getFileCrawlDefinition().setMaxDepth(1), 4, 1, 0);
+        crawlTest(getFileCrawlDefinition().setMaxDepth(2), 6, 2, 0);
+        crawlTest(getFileCrawlDefinition().setMaxDepth(0), 1, 0, 0);
+    }
 }
--- src/test/java/com/qwazr/crawler/file/FileCrawlerTest.java
+++ src/test/java/com/qwazr/crawler/file/FileCrawlerTest.java
@@ -29,104 +29,106 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.SortedMap;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FileCrawlerTest {
 
-	private static FileCrawlerServiceInterface local;
-	private static FileCrawlerServiceInterface remote;
+    private static FileCrawlerServiceInterface local;
+    private static FileCrawlerServiceInterface remote;
 
-	@BeforeClass
-	public static void before() throws Exception {
-		if (CrawlerServer.getInstance() == null)
-			CrawlerServer.main();
-	}
+    @BeforeClass
+    public static void before() throws Exception {
+        if (CrawlerServer.getInstance() == null)
+            CrawlerServer.main();
+    }
 
-	@AfterClass
-	public static void after() {
-		CrawlerServer.shutdown();
-	}
+    @AfterClass
+    public static void after() {
+        CrawlerServer.shutdown();
+    }
 
-	@Test
-	public void test100startServer() throws Exception {
-		local = CrawlerServer.getInstance().getFileCrawlerServiceBuilder().local();
-		Assert.assertNotNull(local);
-		remote = new FileCrawlerSingleClient(RemoteService.of("http://localhost:9091").build());
-		Assert.assertNotNull(remote);
-	}
+    @Test
+    public void test100startServer() throws Exception {
+        local = CrawlerServer.getInstance().getFileCrawlerServiceBuilder().local();
+        Assert.assertNotNull(local);
+        remote = new FileCrawlerSingleClient(RemoteService.of("http://localhost:9091").build());
+        Assert.assertNotNull(remote);
+    }
 
-	@Test
-	public void test200emptySessions() {
-		SortedMap<String, FileCrawlStatus> sessions = remote.getSessions();
-		Assert.assertNotNull(sessions);
-		Assert.assertTrue(sessions.isEmpty());
-	}
+    @Test
+    public void test200emptySessions() {
+        SortedMap<String, FileCrawlStatus> sessions = remote.getSessions();
+        Assert.assertNotNull(sessions);
+        Assert.assertTrue(sessions.isEmpty());
+    }
 
-	private FileCrawlDefinition.Builder getNewCrawl() {
-		return FileCrawlDefinition.of()
-				.entryPath("src/test/file_crawl")
-				.addInclusionPattern("*/")
-				.addInclusionPattern("*.txt")
-				.addExclusionPattern("*/ignore/")
-				.addExclusionPattern("*/ignore.*")
-				.setCrawlWaitMs(10)
-				.setMaxDepth(10);
-	}
+    private FileCrawlDefinition.Builder getNewCrawl() {
+        return FileCrawlDefinition.of()
+                .entryPath(Paths.get("src", "test", "file_crawl").toString())
+                .addInclusionPattern("*" + File.separator)
+                .addInclusionPattern("*.txt")
+                .addExclusionPattern("*" + File.separator + "ignore" + File.separator)
+                .addExclusionPattern("*" + File.separator + "ignore.*")
+                .setCrawlWaitMs(10)
+                .setMaxDepth(10);
+    }
 
-	@Test
-	public void test300SimpleCrawl() throws InterruptedException {
-		final String sessionName = RandomUtils.alphanumeric(10);
-		remote.runSession(sessionName, getNewCrawl().build());
-		final CrawlStatus status = CommonEvent.crawlWait(sessionName, remote);
-		Assert.assertEquals(7, status.crawled);
-		Assert.assertEquals(2, status.ignored);
-		Assert.assertEquals(0, status.error);
-		Assert.assertNull(status.lastError);
-	}
+    @Test
+    public void test300SimpleCrawl() throws InterruptedException {
+        final String sessionName = RandomUtils.alphanumeric(10);
+        remote.runSession(sessionName, getNewCrawl().build());
+        final CrawlStatus status = CommonEvent.crawlWait(sessionName, remote);
+        Assert.assertEquals(7, status.crawled);
+        Assert.assertEquals(2, status.ignored);
+        Assert.assertEquals(0, status.error);
+        Assert.assertNull(status.lastError);
+    }
 
-	@Test
-	public void test400CrawlEvent() throws InterruptedException {
-		FileEvents.feedbacks.clear();
-		final String sessionName = RandomUtils.alphanumeric(10);
-		final FileCrawlDefinition.Builder crawl = getNewCrawl();
-		final String variableName = RandomUtils.alphanumeric(5);
-		final String variableValue = RandomUtils.alphanumeric(6);
-		crawl.script(EventEnum.before_crawl, ScriptDefinition.of(FileEvents.BeforeCrawl.class)
-				.variable(variableName, variableValue + EventEnum.before_crawl.name())
-				.build());
-		crawl.script(EventEnum.after_crawl, ScriptDefinition.of(FileEvents.AfterCrawl.class)
-				.variable(variableName, variableValue + EventEnum.after_crawl.name())
-				.build());
-		crawl.script(EventEnum.before_session, ScriptDefinition.of(FileEvents.BeforeSession.class)
-				.variable(variableName, variableValue + EventEnum.before_session.name())
-				.build());
-		crawl.script(EventEnum.after_session, ScriptDefinition.of(FileEvents.AfterSession.class)
-				.variable(variableName, variableValue + EventEnum.after_session.name())
-				.build());
-		remote.runSession(sessionName, crawl.build());
-		CommonEvent.crawlWait(sessionName, remote);
-		Assert.assertEquals(9, FileEvents.feedbacks.get(EventEnum.before_crawl).count());
-		Assert.assertEquals(7, FileEvents.feedbacks.get(EventEnum.after_crawl).count());
-		Assert.assertEquals(1, FileEvents.feedbacks.get(EventEnum.before_session).count());
-		Assert.assertEquals(1, FileEvents.feedbacks.get(EventEnum.after_session).count());
-		for (EventEnum eventEnum : EventEnum.values())
-			Assert.assertEquals(variableValue + eventEnum.name(),
-					FileEvents.feedbacks.get(eventEnum).variable(variableName));
+    @Test
+    public void test400CrawlEvent() throws InterruptedException {
+        FileEvents.feedbacks.clear();
+        final String sessionName = RandomUtils.alphanumeric(10);
+        final FileCrawlDefinition.Builder crawl = getNewCrawl();
+        final String variableName = RandomUtils.alphanumeric(5);
+        final String variableValue = RandomUtils.alphanumeric(6);
+        crawl.script(EventEnum.before_crawl, ScriptDefinition.of(FileEvents.BeforeCrawl.class)
+                .variable(variableName, variableValue + EventEnum.before_crawl.name())
+                .build());
+        crawl.script(EventEnum.after_crawl, ScriptDefinition.of(FileEvents.AfterCrawl.class)
+                .variable(variableName, variableValue + EventEnum.after_crawl.name())
+                .build());
+        crawl.script(EventEnum.before_session, ScriptDefinition.of(FileEvents.BeforeSession.class)
+                .variable(variableName, variableValue + EventEnum.before_session.name())
+                .build());
+        crawl.script(EventEnum.after_session, ScriptDefinition.of(FileEvents.AfterSession.class)
+                .variable(variableName, variableValue + EventEnum.after_session.name())
+                .build());
+        remote.runSession(sessionName, crawl.build());
+        CommonEvent.crawlWait(sessionName, remote);
+        Assert.assertEquals(9, FileEvents.feedbacks.get(EventEnum.before_crawl).count());
+        Assert.assertEquals(7, FileEvents.feedbacks.get(EventEnum.after_crawl).count());
+        Assert.assertEquals(1, FileEvents.feedbacks.get(EventEnum.before_session).count());
+        Assert.assertEquals(1, FileEvents.feedbacks.get(EventEnum.after_session).count());
+        for (EventEnum eventEnum : EventEnum.values())
+            Assert.assertEquals(variableValue + eventEnum.name(),
+                    FileEvents.feedbacks.get(eventEnum).variable(variableName));
 
-		CommonEvent.Feedback<FileCrawlDefinition, FileCurrentPath> afterFeedback =
-				FileEvents.feedbacks.get(EventEnum.after_crawl);
-		Assert.assertEquals(0, afterFeedback.crawlDepth("src/test/file_crawl/"), 0);
-		Assert.assertEquals(1, afterFeedback.crawlDepth("src/test/file_crawl/file0.txt"), 0);
-		Assert.assertEquals(1, afterFeedback.crawlDepth("src/test/file_crawl/dir1/"), 0);
-		Assert.assertEquals(2, afterFeedback.crawlDepth("src/test/file_crawl/dir1/subdir/"), 0);
-		Assert.assertEquals(3, afterFeedback.crawlDepth("src/test/file_crawl/dir1/subdir/file1.txt"), 0);
-		Assert.assertEquals(1, afterFeedback.crawlDepth("src/test/file_crawl/dir2/"), 0);
-		Assert.assertEquals(2, afterFeedback.crawlDepth("src/test/file_crawl/dir2/file2.txt"), 0);
-		CommonEvent.Feedback<FileCrawlDefinition, FileCurrentPath> beforeFeedback =
-				FileEvents.feedbacks.get(EventEnum.before_crawl);
-		Assert.assertEquals(2, beforeFeedback.crawlDepth("src/test/file_crawl/dir2/ignore.txt"), 0);
-		Assert.assertEquals(1, beforeFeedback.crawlDepth("src/test/file_crawl/ignore/"), 0);
-	}
+        CommonEvent.Feedback<FileCrawlDefinition, FileCurrentPath> afterFeedback =
+                FileEvents.feedbacks.get(EventEnum.after_crawl);
+        Assert.assertEquals(0, afterFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator), 0);
+        Assert.assertEquals(1, afterFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "file0.txt"), 0);
+        Assert.assertEquals(1, afterFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "dir1" + File.separator), 0);
+        Assert.assertEquals(2, afterFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "dir1" + File.separator + "subdir" + File.separator), 0);
+        Assert.assertEquals(3, afterFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "dir1" + File.separator + "subdir" + File.separator + "file1.txt"), 0);
+        Assert.assertEquals(1, afterFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "dir2" + File.separator), 0);
+        Assert.assertEquals(2, afterFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "dir2" + File.separator + "file2.txt"), 0);
+        CommonEvent.Feedback<FileCrawlDefinition, FileCurrentPath> beforeFeedback =
+                FileEvents.feedbacks.get(EventEnum.before_crawl);
+        Assert.assertEquals(2, beforeFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "dir2" + File.separator + "ignore.txt"), 0);
+        Assert.assertEquals(1, beforeFeedback.crawlDepth("src" + File.separator + "test" + File.separator + "file_crawl" + File.separator + "ignore" + File.separator), 0);
+    }
 
 }
